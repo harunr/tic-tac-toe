c2abe49b8b4e9fc8e72c17fcded797b8e74223e9
ronycsedev <ronywebtricker@gmail.com>
2022-04-20 11:55:03 +0600

    tic-tac-toe

---
+++ precache-manifest.1f9cb6ef1975462a16ea3c48b2f66420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3754b93a69d2eee704d9e7ddf3cd8df4",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "89e6334ac7ac579e2201",
+    "url": "/tic-tac-toe/static/css/main.360174e2.chunk.css"
+  },
+  {
+    "revision": "a1edb47e95d3db503c3c",
+    "url": "/tic-tac-toe/static/js/2.e867a104.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.e867a104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e6334ac7ac579e2201",
+    "url": "/tic-tac-toe/static/js/main.3a448b3c.chunk.js"
+  },
+  {
+    "revision": "28dfba9420967d301740",
+    "url": "/tic-tac-toe/static/js/runtime-main.4c12a8d1.js"
+  }
+]);
